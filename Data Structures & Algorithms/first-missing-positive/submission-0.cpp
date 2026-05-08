b0d94e8932aaea9cc33144d0fc52f8578525e251
veerbhansinghtomar045-bit <veerbhansinghtomar045@gmail.com>
2026-05-08 20:00:51 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+
+        int missing =1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==missing){
+                missing++;
+            }
+        }
+
+        
+   return missing;
+    }
+};
