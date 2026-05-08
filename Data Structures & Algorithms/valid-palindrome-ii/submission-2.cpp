8ad607ce273d50589960c6253370da1d8618b7e5
veerbhansinghtomar045-bit <veerbhansinghtomar045@gmail.com>
2026-05-08 15:56:30 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkPalindrome(string s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return checkPalindrome(s, left + 1, right) ||
+                       checkPalindrome(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};    
+    
